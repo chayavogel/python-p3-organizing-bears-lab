39a92f6d8328f5b77cc3dda391c7aa989a06f1db
Chaya Vogel <chayavogel1@gmail.com>
2023-12-05 10:28:20 -0500

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Black", "Greedy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Brown", "Rowdy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Red", "Sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 5, "M", "Brown", "Bossy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "Orange", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 3, "M", "Yellow", "Restless", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "F", "Green", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, "M", "Blue", "Gentle", 1);
